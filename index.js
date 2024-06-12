06dac8ad9d3efbfdb577d6d5e0e3ca82d13ed2bc
Abhishek Bhatt <abhishekbhatti625@gmail.com>
2024-06-12 15:33:25 +0545

    Variables in js

---
+++ index.js
@@ -0,0 +1,40 @@
+// variable = A container that stores a value.
+        //    Behaves as if it were  the value it contains.
+
+// 1. Declaration let x;
+// 2. Assgnment x = 100;
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`You have got ${gpa} gpa in boards `);
+
+
+// let firstName ="Abhi";  
+// let favouriteFood ="pizza";
+// let email ="Abhi1241@'gmail.com";
+
+
+// console.log(typeof age);
+// console.log(`your name is ${firstName}`);
+// console.log(`your favourite food is ${favouriteFood}`);
+// console.log(`your email is ${email}`);
+
+// let online = true;
+// let forSale = false;
+// let isStudent = true;
+
+// console.log(`Abhi is online: ${online}`);
+// console.log(`Is this car for sale: ${forSale}`);
+// console.log(`Is he a student: ${isStudent}`);
+
+let fullName = "Abhi";
+let age = "22";
+let istudent = "True";
+
+document.getElementById("p1").textContent= (`Your name is ${fullName}`);
+document.getElementById("p2").textContent= (`You are ${age} years old.`);
+document.getElementById("p3").textContent= (`Yor are a student: ${istudent}`);
